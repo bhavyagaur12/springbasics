8b69902a6f05d74d9154ce595ad9342ee108fa57
PRIYANSHU <PRIYANSHU@DESKTOP-1P809VR>
2018-12-15 19:36:24 +0530

    first commit

---
+++ prototype/src/demo1/Client.java
@@ -0,0 +1,27 @@
+package demo1;
+
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Client {
+
+	public static void main(String[] args) {
+		System.out.println("main");
+	ApplicationContext context= new ClassPathXmlApplicationContext("config.xml");
+	
+	ReportService service1=(ReportService) context.getBean("ReportService");
+	ReportService service2=(ReportService) context.getBean("ReportService");
+	if(service1==service2) {
+		System.out.println("same instance in singleton");
+	}
+	else{
+		System.out.println("different instance PROTOTYPE");
+		
+	}
+//	service1.display();
+	
+	
+	}
+}
