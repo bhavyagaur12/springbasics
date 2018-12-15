15e38ab540e1399e39424e2f77c49274bbaed0de
PRIYANSHU <PRIYANSHU@DESKTOP-1P809VR>
2018-12-15 19:37:05 +0530

    first commit

---
+++ annotation_autowire_OnSetterMethod/src/demo1/ReportService.java
@@ -0,0 +1,42 @@
+package demo1;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+
+public class ReportService {
+
+	
+	public ReportGenerator master;
+	
+	public int records;
+	
+	public ReportService(ReportGenerator master,int records) {
+		System.out.println("parameterized constructor");
+		this.master=master;
+		this.records=records;
+	}
+	
+	public ReportService() {
+		System.out.println("this is the default constructor this will work when there is only one bean definition of similiar type");
+	}
+	
+	public ReportGenerator getMaster() {
+		return master;
+	}
+	@Autowired
+	public void setMaster(ReportGenerator master) {
+		System.out.println("inside the setter method of master");
+		this.master = master;
+	}
+	public int getRecords() {
+		return records;
+	}
+	public void setRecords(int records) {
+		this.records = records;
+	}
+	
+	public void display() {
+	master.display(records);
+	}
+	
+}
