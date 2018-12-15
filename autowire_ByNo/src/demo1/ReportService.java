0b9819d94ceaac330dbc894d7ce89be334304699
PRIYANSHU <PRIYANSHU@DESKTOP-1P809VR>
2018-12-15 19:34:43 +0530

    first commit

---
+++ autowire_ByNo/src/demo1/ReportService.java
@@ -0,0 +1,36 @@
+package demo1;
+
+public class ReportService {
+
+	public ReportGenerator master;
+	public int records;
+	
+//	public ReportService(ReportGenerator master,int records) {
+//		System.out.println("parameterized constructor");
+//		this.master=master;
+//		this.records=records;
+//	}
+	
+//	public ReportService() {
+//		System.out.println("this is the default constructor");
+//	}
+	
+	public ReportGenerator getMaster() {
+		return master;
+	}
+	public void setMaster(ReportGenerator master) {
+		System.out.println("inside the setter method of master");
+		this.master = master;
+	}
+	public int getRecords() {
+		return records;
+	}
+	public void setRecords(int records) {
+		this.records = records;
+	}
+	
+	public void display() {
+	master.display(records);
+	}
+	
+}
